75802d54effa144a256d376c441b2b3a2760be8b
Pol REITZ <reitz.pol@gmail.com>
2022-06-11 15:35:23 +0200

    on tente encore

--- server.js
+++ server.js
@@ -5,11 +5,12 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist'));
+app.use(express.static(__dirname + 'build'));
 
 app.get('/*', function(req,res) {
-  const index = path.join(__dirname, 'dist', 'index.html');
-  res.sendFile(index);
+  /*const index = path.join(__dirname, 'dist', 'index.html');
+  res.sendFile(index);*/
+  res.sendFile('index.html');
 });
 
 // Start the app by listening on the default Heroku port
