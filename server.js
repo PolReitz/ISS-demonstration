bd6425655ab9a18450c714b94d777b97ba6c8b2d
Pol REITZ <reitz.pol@gmail.com>
2022-06-11 16:06:07 +0200

    c'est la bonne ?

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const app = express();
 // Serve only the static files form the dist directory
 app.use(express.static(__dirname + '/dist/angular-issdemo'));
 app.get('/*', function(req,res) {
-  const fullPath = path.join(__dirname +'/dist/music-app/');
+  const fullPath = path.join(__dirname +'/dist/angular-issdemo/index.html');
   console.log(" Fetching from.." + fullPath);
     res.sendFile(fullPath);
 });
