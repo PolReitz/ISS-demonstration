522ba63bd46ea088fa6f791a1b6ac966100dc68b
Pol REITZ <reitz.pol@gmail.com>
2022-06-11 22:51:00 +0200

    teest

--- server.js
+++ server.js
@@ -5,11 +5,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname));// + '/dist/angular-issdemo'));
+app.use(express.static(__dirname + '/dist/angular-issdemo'));
 
 app.get('/*', function(req,res) {
     
-res.sendFile(path.join(__dirname+'/index.html'));//'/dist/angular-issdemo/index.html'));
+res.sendFile(path.join(__dirname+'/dist/angular-issdemo/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
