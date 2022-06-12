81f7611900da346067d45ccaf1d358e34da80873
Pol REITZ <reitz.pol@gmail.com>
2022-06-12 21:49:51 +0200

    test

--- server.js
+++ server.js
@@ -6,15 +6,15 @@ const app = express();
 
 // Serve only the static files form the dist directory
 //app.use(express.static(__dirname + '/dist/angular-issdemo'));
-/*app.use(express.static(path.join(__dirname, 'dist','angular-issdemo')));
+app.use(express.static(path.join(__dirname, 'dist','angular-issdemo')));
 
 app.get('/*', function(req,res) {
   //res.sendFile(path.join(__dirname+'/dist/angular-issdemo/index.html'));
   res.sendFile(path.join(__dirname,'dist','angular-issdemo','index.html'));
-});*/
+});
 
 // Deployment
-if (process.env.NODE_ENV === "production") {
+/*if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../client/build")));
   app.get("*", (req, res) =>
     // res.sendFile(path.resolve(__dirname, "../client", "public", "index.html"))
@@ -24,7 +24,7 @@ if (process.env.NODE_ENV === "production") {
   app.get("/", (req, res) => {
     res.send("API IS RUNNING.");
   });
-}
+}*/
 
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
